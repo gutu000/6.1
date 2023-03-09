006c3d4692d87fce282ab3e7ad23d577ae0c0a79
Admin <Admin@TEB106008>
2023-03-09 13:39:33 +0100

    Add project files.

---
+++ gropbfpgifyggt/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Hej
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            int summan = Addera(5, 15, 3);
+            Console.WriteLine(summan);
+        }
+        static int Addera(int tal1, int tal2, int tal3)
+        {
+            return tal1 + tal2 + tal3;
+        }
+    }
+}
